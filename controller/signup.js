66deafff121ccc57db913f284abea5e47f7005a0
Ayine-nongre <eugeneatinbire@gmail.com>
2023-11-06 14:31:53 +0000

    Adding functionality to register a new user

---
+++ controller/signup.js
@@ -0,0 +1,30 @@
+import bcrypt from 'bcrypt'
+import User from '../model/user.js'
+
+
+export const signup = async (req, res) => {
+    const data = req.body
+
+    if (!data) return res.json( { message: "No data received from frontend" })
+
+    const { name, email, password, confirmPassword } = data
+
+
+    if (password != confirmPassword) return res.json( { message: "Passwords do not match" })
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    if (!hashedPassword) return res.status(500).json({ message: "Internal server error"})
+
+    const user = await User.create({
+        name: name,
+        email: email,
+        password: hashedPassword
+    })
+
+    res.status(201).json({
+        Username: name,
+        Email: email,
+        status: "Success"
+    })
+}
